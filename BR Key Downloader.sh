a10e449355e9c9b69fbdd736255d16797b153c29
CByte10 <56134763+CByte10@users.noreply.github.com>
2020-04-30 14:49:35 +0100

    Changed file name to be unzipped

--- BR Key Downloader.sh
+++ BR Key Downloader.sh
@@ -2,4 +2,4 @@ mkdir ~/.config/aacs ; #create directory incase it isn't already there
 cd ~/.config/aacs ;
 wget http://fvonline-db.bplaced.net/export/keydb_eng.zip ;
 #mv fv_download.php?lang=eng key.zip ; # renames to a zip file
-unzip key.zip ;
+unzip keydb_eng.zip ;

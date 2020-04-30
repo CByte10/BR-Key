04e2dffca0b8e8f237a669e03e4caa4e74eeb517
CByte10 <56134763+CByte10@users.noreply.github.com>
2020-04-30 14:56:21 +0100

    Update BR Key Downloader.sh

--- BR Key Downloader.sh
+++ BR Key Downloader.sh
@@ -1,7 +1,6 @@
 mkdir ~/.config/aacs ; #create directory incase it isn't already there
 cd ~/.config/aacs ;
-rm keydb_eng.zip ; # removes old files
-rm keydb.cfg ; 
+rm keydb_eng.zip keydb.cfg ; # removes old files
 wget http://fvonline-db.bplaced.net/export/keydb_eng.zip ;
 #mv fv_download.php?lang=eng key.zip ; # renames to a zip file
 unzip keydb_eng.zip ;

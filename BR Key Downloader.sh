24a6b7806aaa61c2be7b587883ab664b97cfde2f
CByte10 <56134763+CByte10@users.noreply.github.com>
2020-04-30 14:42:26 +0100

    Comments added

--- BR Key Downloader.sh
+++ BR Key Downloader.sh
@@ -1,4 +1,5 @@
+mkdir ~/.config/aacs ; #create directory incase it isn't already there
 cd ~/.config/aacs ;
 wget http://fvonline-db.bplaced.net/fv_download.php?lang=eng ;
-mv fv_download.php?lang=eng key.zip ;
+mv fv_download.php?lang=eng key.zip ; # renames to a zip file
 unzip key.zip ;

d8c0723387f082a38b811dcd1d0aa90b447fae4f
CByte10 <56134763+CByte10@users.noreply.github.com>
2020-05-11 15:00:43 +0100

    Update BR Key Downloader.sh

--- BR Key Downloader.sh
+++ BR Key Downloader.sh
@@ -1,5 +1,6 @@
-mkdir ~/.config/aacs ; #create directory incase it isn't already there
+mkdir ~/.config/aacs ; #create directory incase it isn't already there, if it is it will print out an error, but will still be ok
 cd ~/.config/aacs ;
-rm keydb_eng.zip keydb.cfg ; # removes old files
+rm keydb_eng.zip keydb.cfg KEYDB.cfg ; # removes old files
 wget http://fvonline-db.bplaced.net/export/keydb_eng.zip ;
 unzip keydb_eng.zip ;
+mv keydb.cfg KEYDB.cfg # renames it to upper case
